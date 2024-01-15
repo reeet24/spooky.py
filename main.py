a67ec029988bc698cf83cfec572b9a8a7c199a08
Spooky.ico <metbetset@outlook.com>
2024-01-15 16:37:10 -0500

    Added logging for future bugfixes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from UtilsDirectory.data import *
+import logging
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 @bot.event
 async def on_ready():
@@ -45,4 +48,4 @@ async def reload(ctx, choice = None):
         await ctx.send('Invalid Args')
 
 
-bot.run(secret_token)
+bot.run(secret_token, log_handler=handler)
